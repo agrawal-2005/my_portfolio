3896896d84825c07ccd1cc5cdfb5e59995fbc5e6
PrashantAgrawal <agrawalprashant906@gamil.com>
2024-05-20 13:49:54 +0530

    google form

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ links.forEach((link, index) => {
     contents[index].classList.add('active-tab');
   });
 });
+
+document.querySelector('#menuToggle').addEventListener('click', ()=> {
+    let menu = document.getElementById('menu');
+    menu.classList.toggle('right-[0px]');
+});
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx23I-GlToKWFmTg-qGHXOF1yD1q2T3oq2BPr0kuSCXzHxkFo0X6kCUm_5ljfjfJli8YA/exec'
+const form = document.forms['submit-to-google-sheet']
+
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => console.log('Success!', response))
+    .catch(error => console.error('Error!', error.message))
+})
